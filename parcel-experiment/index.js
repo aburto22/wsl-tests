ee39fd30a1be45382ee061b54d0ccf2b080f83c6
aburto22 <aburto22@gmail.com>
2021-03-18 13:41:27 -0400

    commit

--- parcel-experiment/index.js
+++ parcel-experiment/index.js
@@ -1,1 +1,6 @@
 console.log("Hello World");
+
+import { formatDistanceToNow } from "date-fns";
+
+const date = "1996-09-13 10:00:00";
+document.body.textContent = formatDistanceToNow(new Date(date)) + " ago";
