ed790e3f167967bd17890210693e601ee2b76e1f
aburto22 <aburto22@gmail.com>
2021-04-12 17:22:52 -0400

    commit

--- euler/euler.js
+++ euler/euler.js
@@ -2,7 +2,7 @@ function divisibleTriangleNumber(n) {
   const getDiv = (num) => {
     if (num == 1) return 1;
     const divs = [1, num];
-    for (let i = 2; i < num / 2; i++) {
+    for (let i = 2; i <= num / 3; i++) {
       if (num % i == 0 && !divs.includes(i)) {
         divs.push(i);
         divs.push(num / i);
@@ -17,33 +17,58 @@ function divisibleTriangleNumber(n) {
   };
 
   const checkTriang = (num) => {
-    for (let i = 1; num > 0; i++) {
+    let i = 1;
+    for (i = 1; num > 0; i++) {
       num -= i;
     }
 
-    return num == 0;
+    if (num == 0) return i - 1;
+    else return 0;
   };
 
-  let min = 0;
-  let max = 10000;
+  const getMinTriang = (num) => {
+    let index = checkTriang(num);
+    while (index === 0) {
+      num++;
+      index = checkTriang(num);
+    }
 
-  while (getDiv(max) < n) {
-    min += 10000;
-    max += 10000;
-  }
+    return [num, index];
+  };
 
   let num = 0;
+  let count = 1;
 
-  for (let i = min; i < max; i++) {
-    if (checkTriang(i)) {
-      if (getDiv(i) > n) {
-        num = i;
-        break;
-      }
+  for (let i = 1; i < 15000; i++) {
+    num = num + i;
+    count = i;
+    if (getDiv(num) > n) break;
+  }
+
+  if (getDiv(num) < n) {
+    for (let i = count; i < 15000; i++) {
+      num = num + i;
+      count = i;
+      if (getDiv(num) > n) break;
     }
   }
+
+  //console.log(getMinTriang(76576500));
+  console.log(n);
+  console.log(num);
+  console.log(count);
 
   return num;
 }
 
-console.log(divisibleTriangleNumber(500));
+divisibleTriangleNumber(500);
+
+const check = (num) => {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  console.log(sum);
+};
+
+//check(12375)
